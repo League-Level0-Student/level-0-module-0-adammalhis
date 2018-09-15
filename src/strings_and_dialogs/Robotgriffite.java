e567d912eba938ba8491fc0ca0a437409b188ea1
adammalhis <36518130+adammalhis@users.noreply.github.com>
2018-09-15 16:18:58 -0700

    robotsquare robotgraffiti morningzombie

---
+++ src/strings_and_dialogs/Robotgriffite.java
@@ -0,0 +1,20 @@
+package strings_and_dialogs;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Robotgriffite {
+public static void main(String[] args) {
+	Robot Bob= new Robot(); 
+	Bob.penDown();
+	Bob.move(200);
+	Bob.turn(90);
+	Bob.move(100);
+	Bob.turn(90);
+	Bob.move(200);
+	Bob.turn(180);
+	Bob.move(100);
+	Bob.turn(270);
+	Bob.move(100);
+	
+}
+}
---
+++ src/strings_and_dialogs/morningZombie.java
@@ -0,0 +1,11 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class morningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "wake up");
+	JOptionPane.showMessageDialog(null,"get out of bed");
+	JOptionPane.showMessageDialog(null, "get out of room");
+}
+}
