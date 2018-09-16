efe8efd20d56c309cba01dffa882412e47ec786b
League <league@WTS9.attlocal.net>
2018-09-16 13:00:46 -0700

    Las summer

---
+++ src/Lastsummer.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Lastsummer {
+public static void main(String[] args) {
+	String Therename=JOptionPane.showInputDialog(null,"what is your name?");
+	JOptionPane.showMessageDialog(null, "I know that you swam last summer, "+Therename);
+}
+}
